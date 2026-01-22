ee3e93d6d7da2714fb7de0e8a39d412c0b437042
cgaspara <cgaspara@gmail.com>
2026-01-22 10:00:40 -0800

    Add AWS S3 bucket configuration for SQL backups

--- aws/dev/us-west-2/s3-sql-backup/main.tf
+++ aws/dev/us-west-2/s3-sql-backup/main.tf
@@ -1,1 +1,31 @@
-.
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_s3_bucket" "sql_backup" {
+  bucket = "mssql-backup-${random_id.bucket_suffix.hex}"
+  force_destroy = true
+
+  tags = {
+    Name        = "mssql-backup"
+    Environment = "dev"
+    Owner       = "cgaspara"
+  }
+}
+
+resource "random_id" "bucket_suffix" {
+  byte_length = 4
+}
+
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.sql_backup.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+output "bucket_name" {
+  value = aws_s3_bucket.sql_backup.bucket
+}
+
